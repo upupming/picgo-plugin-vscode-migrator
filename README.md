c98293c1e81d0e0af1e63f58a6177b6e2b0ed690
upupming <upupming@gmail.com>
2019-04-02 01:53:58 +0800

    Finished: basic importing

---
+++ README.md
@@ -0,0 +1,48 @@
+## picgo-plugin-vscode-migrator
+
+<a href="https://www.npmjs.com/package/picgo-plugin-vscode-migrator"><img src="https://img.shields.io/npm/v/picgo-plugin-vscode-migrator.svg?style=flat-square" alt="npm"></a>
+<a href="https://standardjs.com"><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg?style=flat-square" alt="JavaScript Style Guide"></a>
+<a href="https://travis-ci.com/upupming/picgo-plugin-vscode-migrator/builds"><img src="https://img.shields.io/travis/com/upupming/picgo-plugin-vscode-migrator.svg?style=popout-square" alt="travis build status"></a>
+<a href="https://github.com/upupming/picgo-plugin-vscode-migrator/blob/master/LICENSE"><img src="https://img.shields.io/github/license/mashape/apistatus.svg?style=popout-square" alt="License"></a>
+
+Import images from `vs-picgo` to PicGo.
+
+## Step 1: upload images using [`vs-picgo`](https://github.com/Spades-S/vs-picgo/)
+
+![vs-picgo-demo.gif](https://i.loli.net/2019/04/02/5ca25272d0e84.gif)
+
+## Step 2: import images from `vs-picgo`
+
+Before importing:
+
+![20190402020419.png](https://i.loli.net/2019/04/02/5ca252a4d1eb6.png)
+
+`C:\Users\Doraeming\vs-picgo-log.json`(`vs-picgo` log path for Windows):
+
+```json
+{
+  "smms": [
+    {
+      "fileName": "vs-picgo-demo.gif",
+      "width": 1030,
+      "height": 776,
+      "extname": ".gif",
+      "imgUrl": "https://i.loli.net/2019/04/02/5ca253456c4bd.gif",
+      "type": "smms",
+      "id": "f37b4678-ea3b-402d-be64-7fd011cc32a8"
+    }
+  ]
+}
+```
+
+Now run `picgo-plugin-vscode-migrator` to import:
+
+![vscode-migrator.gif](https://i.loli.net/2019/04/02/5ca2565f51930.gif)
+
+After imported:
+
+`C:\Users\Doraeming\vs-picgo-log.json`:
+
+```json
+{}
+```
---
+++ src/index.ts
@@ -0,0 +1,100 @@
+import picgo from 'picgo'
+import { Notification } from 'electron'
+import { readFileSync, writeFileSync } from 'fs'
+import os from 'os'
+import path from 'path'
+
+const vsLogPath = path.resolve(os.homedir(), 'vs-picgo-log.json')
+
+function showNotificaiton(guiApi, text: string) {
+  // guiApi.showNotificaiton({
+  //   title: 'vscode-migrator',
+  //   body: text
+  // })
+  const notification = new Notification({
+    title: 'vscode-migrator',
+    body: text
+  })
+  notification.show()
+}
+
+function syntaxError(guiApi) {
+  showNotificaiton(guiApi, `The vs-picgo log file ${vsLogPath} has syntax error, ` + 
+  `please fix the error by yourself`)
+}
+function fileNotExists(guiApi) {
+  showNotificaiton(guiApi, `The vs-picgo log file ${vsLogPath} doesn't exists`)
+}
+
+function getPicgoImagesJson(ctx: picgo, guiApi) {
+  try {
+    const picgoConfigPath = path.resolve(ctx.baseDir, 'data.json')
+    let data = JSON.parse(readFileSync(picgoConfigPath, 'utf8'))
+    return data.uploaded
+  } catch (err) {
+    showNotificaiton(guiApi, 'Read PicGo config failed')
+    console.error(err)
+  }
+}
+
+function writePicgoImagesJson(ctx: picgo, guiApi, uploaded) {
+  try {
+    const picgoConfigPath = path.resolve(ctx.baseDir, 'data.json')
+    let data = JSON.parse(readFileSync(picgoConfigPath, 'utf8'))
+    data.uploaded = uploaded
+    writeFileSync(picgoConfigPath, JSON.stringify(data, null, 2), 'utf8')
+  } catch (err) {
+    showNotificaiton(guiApi, 'Write PicGo config failed')
+    console.error(err)
+  }
+}
+
+function reCreateVsLogFile() {
+  writeFileSync(vsLogPath, JSON.stringify({}))
+}
+
+const migrateFromVsPicgo = {
+  label: 'Import images from vs-picgo',
+  async handle (ctx: picgo, guiApi) {
+    try {
+      showNotificaiton(guiApi, 'Started importing images from vs-picgo')
+      let data = readFileSync(vsLogPath, 'utf8')
+      let log = JSON.parse(data)
+      let picgoUploaded = getPicgoImagesJson(ctx, guiApi)
+      let imagesImported = 0
+      Object.keys(log).forEach(picBed => {
+        picgoUploaded = picgoUploaded.concat(log[picBed])
+        imagesImported += log[picBed].length
+      })
+      writePicgoImagesJson(ctx, guiApi, picgoUploaded)
+      reCreateVsLogFile()
+      showNotificaiton(guiApi, `Successfully imported ${imagesImported} images from vs-picgo`)
+    } catch (err) {
+      if (err.message.includes('ENOENT: no such file or directory')) {
+        fileNotExists(guiApi)
+      }
+      else if (err instanceof SyntaxError) {
+        syntaxError(guiApi)
+      }
+      else {
+        showNotificaiton(
+          guiApi,
+          `Error happened when importing: ${err || ''}`
+        );
+        console.error(err);
+      }
+    } 
+  }
+}
+
+const guiMenu = ctx => {
+  return [migrateFromVsPicgo]
+}
+
+module.exports = (ctx) => {
+  const register = () => {}
+  return {
+    register,
+    guiMenu
+  }
+}
